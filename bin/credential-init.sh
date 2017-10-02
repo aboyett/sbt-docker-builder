a1ded851a7b5482a378e37009f2d4008226f80eb
Andy Boyett <andy@cloudphysics.com>
2017-10-02 16:38:37 -0700

    refactor credentials setup to use $SBT_CREDENTIALS
    sbt can be supplied with a credentials property file via the
    `SBT_CREDENTIALS` environment variable since 0.11.3[1]. This feature is
    unfortunately undocumented and was discovered reading the SBT source
    code.
    
    The credential initialization has also been reworked to move the setup
    sentinel into `bin/credential-init.sh`. This simplifies the sbt wrapper
    itself, allowing the module to always be sourced, thereby ensuring the
    `SBT_CREDENTIALS` env var is always set.
    
    [1] https://github.com/sbt/sbt/commit/96e5a7957c830430f85b6b89d7bbe07824ebfc4b

--- bin/credential-init.sh
+++ bin/credential-init.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env sh
 
-# many internal projects use ~/.ivy/.credentials to store artifact repo creds
-# this hackery pulls the credentials from the CI environement but ensures they
-# aren't cached along with the ivy cache
+CI_CREDENTIAL_SENTINEL="${HOME}/.ci-creds-stamp"
+SBT_CREDENTIAL_FILE=${HOME}/.credentials/sbt-credentials
 
-SBT_CREDENTIAL_FILE=${HOME}/.ivy2/.credentials
-REAL_CREDENTIAL_FILE=${REAL_HOME}/.credentials/sbt-credentials
+# setup credentials and touch sentinel file
+if [ ! -f "${CI_SETUP_SENTINEL}" ] ; then
+    if [ -n "${ARTIFACTORY_USER}" -a -n "${ARTIFACTORY_PASSWORD}" -a -n "${ARTIFACTORY_HOST}" ] ; then
+        echo -n "Creating Artifactory credentials... "
+        mkdir -p $(dirname ${SBT_CREDENTIAL_FILE})
 
-if [ -n "${ARTIFACTORY_USER}" -a -n "${ARTIFACTORY_PASSWORD}" -a -n "${ARTIFACTORY_HOST}" ] ; then
-    echo -n "Creating Artifactory credentials... "
-    mkdir -p ${HOME}/.ivy2 $(dirname ${REAL_CREDENTIAL_FILE})
-
-    ln -sf "${REAL_CREDENTIAL_FILE}" "${SBT_CREDENTIAL_FILE}"
-
-    cat > ${REAL_CREDENTIAL_FILE} << EOFEOFEOF
+        cat > ${SBT_CREDENTIAL_FILE} << EOFEOFEOF
 realm=Artifactory Realm
 host=${ARTIFACTORY_HOST}
 user=${ARTIFACTORY_USER}
 password=${ARTIFACTORY_PASSWORD}
 EOFEOFEOF
-    echo "Done!"
-else
-    echo "Skipping Artifactory credential creation. \$ARTIFACTORY_USER and \$ARTIFACTORY_PASSWORD are not set."
+        echo "Done!"
+    else
+        echo "Skipping Artifactory credential creation. \$ARTIFACTORY_USER and \$ARTIFACTORY_PASSWORD are not set."
+    fi
+
+    touch ${CI_CREDENTIAL_SENTINEL}
+fi
+
+# sbt will read credentials from a file if the path is set in SBT_CREDENTIALS
+# https://github.com/sbt/sbt/commit/96e5a7957c830430f85b6b89d7bbe07824ebfc4b
+if [ -f "${SBT_CREDENTIAL_FILE}" ] ; then
+    export SBT_CREDENTIALS=${SBT_CREDENTIAL_FILE}
 fi
